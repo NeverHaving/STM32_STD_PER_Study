123bc0d6a98ac000213b681f4a95c11f4dd21ab6
NeverHaving <1973250992@qq.com>
2024-12-05 22:43:37 +0800

    Add STM32F407ZGT6 Template

---
+++ STM32F4_PROJECT_EXAMPLE/User/main.c
@@ -0,0 +1,23 @@
+#include "stm32f4xx.h"
+
+static __IO uint32_t uwTimingDelay;
+
+int main ()
+{
+	while (1);
+}
+
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (uwTimingDelay != 0x00)
+  { 
+    uwTimingDelay--;
+  }
+}
+
